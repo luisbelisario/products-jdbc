fe44fbec2fb3ae4709dc0e29bf92c1d930b8bed1
luisbelisario <luisbls77@hotmail.com>
2023-08-14 10:20:35 -0300

    update comments

--- src/main/java/org/belisario/products/jdbc/util/DatabaseConnection.java
+++ src/main/java/org/belisario/products/jdbc/util/DatabaseConnection.java
@@ -11,6 +11,8 @@ public class DatabaseConnection {
     private static final String password = "root";
     private static Connection connection;
 
+    // Aqui estamos trabalhando com o padrão singleton
+    // Estou garantindo que sempre será usado o mesmo objeto para conectar à base de dados
     public static Connection getConnectionInstance() throws SQLException {
         if(connection == null) {
             connection = DriverManager.getConnection(url, username, password);
